b26764180bcf1eb3d7de88e3ceb25808d48a2115
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-05-27 21:07:32 -0300

    primieor dia de exercicios do bloco 5

---
+++ exercises/javascript-dom-eventos-e-web-storage_1/exercice_1.js
@@ -0,0 +1,21 @@
+      
+      
+      
+document.getElementsByTagName('p')[1].innerText = " Daqui a dois anos me vejo no mercado de desenvolvimento ";
+
+document.getElementsByClassName("main-content")[0].style.backgroundColor="rgb(76,164,109)";
+
+document.getElementsByTagName('div')[1].style.backgroundColor="rgb(255, 255, 255)";
+
+document.getElementsByTagName('h1')[0].innerText="Exercício 5.1 - JavaScript";
+
+document.getElementsByTagName('p')[1].style.textTransform= "uppercase";
+
+
+// for (let index = 0 ; index < document.getElementsByTagName('p')[index]){
+//   console.log(document.getElementsByTagName('p')[index].innerText)
+// }
+
+console.log(document.getElementsByTagName('p')[0].innerText)
+console.log(document.getElementsByTagName('p')[1].innerText)
+console.log(document.getElementsByTagName('p')[2].innerText)
